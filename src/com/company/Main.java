5a36756ad7c0d02463df8b5fb2e4fcd251cbd842
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-03-18 19:41:39 +0100

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("saisissez un age ");
+        boolean test = scanner.hasNextInt();
+
+        while(test == false){
+            System.out.println("vous n'avez pas saisi un nombre");
+            test = scanner.hasNextInt();
+        }
+        int valeur = scanner.nextInt();
+        System.out.println(valeur);
     }
 }
