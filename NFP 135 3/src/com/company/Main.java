f7f18d4ad5df6d108b2e1359616dbb6d0ef8147d
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-03-22 23:38:58 +0100

    exo

--- NFP 135 3/src/com/company/Main.java
+++ NFP 135 3/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Veuillez saisir le nombre d'étudiants: ");
+        int nEtudiants = scanner.nextInt();
+
+        System.out.println("Veuillez saisir le nombre de notes par étudiants: ");
+        int nNotes = scanner.nextInt();
+
+        int[][] notes = new int[nEtudiants][nNotes];
+        
     }
 }
