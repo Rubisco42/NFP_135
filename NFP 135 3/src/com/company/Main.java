efff20858d88168facc0b2dee3783e23b76dd8c1
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-03-23 18:17:57 +0100

    gné

--- NFP 135 3/src/com/company/Main.java
+++ NFP 135 3/src/com/company/Main.java
@@ -1,35 +1,6 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
-
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Veuillez saisir le nombre d'étudiants: ");
-        int nEtudiants = scanner.nextInt();
-
-        System.out.println("Veuillez saisir le nombre de notes par étudiants: ");
-        int nNotes = scanner.nextInt();
-
-        double[][] promo = new double[nEtudiants][nNotes];
-
-        float note = -1;
-
-        for(int i = 0; i < nEtudiants; i++){
-            System.out.println("Veillez saisir les notes de l'étudiant "+(i+1)+": ");
-
-            for(int j = 0; j< nNotes; j++){
-                System.out.println("note "+(j+1));
-                note = scanner.nextFloat();
-                promo[i][j]=note;
-            }
-
-        }
-
 
 
     }
-}
