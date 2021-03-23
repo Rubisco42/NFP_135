120aa3c5425ea9bf1c600fe74dc9d224ff6359d8
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-03-23 22:44:01 +0100

    gnéééé

--- NFP 135 3/src/com/company/Exo_notes.java
+++ NFP 135 3/src/com/company/Exo_notes.java
@@ -13,8 +13,11 @@ public class Exo_notes {
         System.out.println("Veuillez saisir le nombre de notes par étudiants: ");
         int nNotes = scanner.nextInt();
 
-        double[][] promo = new double[nEtudiants][nNotes];
+        float[][] pRomo = new float[nEtudiants][nNotes];
+        float[] cumEtudiant = new float[nNotes];
+        float[] cumPromo = new float [nEtudiants];
 
+        float cumNotes = 0;
         float note = -1;
 
         for(int i = 0; i < nEtudiants; i++){
@@ -23,8 +26,14 @@ public class Exo_notes {
             for(int j = 0; j< nNotes; j++){
                 System.out.println("note "+(j+1));
                 note = scanner.nextFloat();
-                promo[i][j]=note;
+                pRomo[i][j]=note;
+                cumEtudiant[j] = note;
             }
+            for(float valeur : cumEtudiant){
+                cumNotes +=valeur;
+            }
+            float moyEtudiant = cumNotes/nNotes;
+            System.out.println("l'étudiant "+(i+1)+" a " + moyEtudiant + " de moyenne");
 
         }
 
