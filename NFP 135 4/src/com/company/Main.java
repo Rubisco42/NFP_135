91f319a81e8fc570fe6d0982e6dd7c94f96dbe39
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-03-26 18:47:07 +0100

    init seance 5

---
+++ NFP 135 4/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        maMethode();
+        maMethode();
+        maMethode();
+	// write your code here
+    }
+    public static void maMethode(){
+        System.out.println("Hello world");
+    }
+}
